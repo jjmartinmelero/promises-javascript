6ef7cf739880c6cc5b6022b766dd4704e8ac9f4d
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-09 08:21:30 +0000

    added example promise in promise.js file

--- promise.js
+++ promise.js
@@ -1,8 +1,23 @@
+const promesaResuelta = Promise.resolve(5);
+console.log(promesaResuelta); // Promise { 5 }
 
+const promesaRechazada = Promise.reject("Error");
+promesaRechazada.catch((err) =>
+  console.log("Promesa rechazada capturada:", err),
+);
+console.log(promesaRechazada); // Promise { <rejected>: "Error" }
 
-const promesaResuelta = Promise.resolve(5)
-console.log(promesaResuelta) // Promise { 5 }
+// Example to create a promise
+function promiseFriendship() {
+  return new Promise((resolve, reject) => {
+    if (Math.random() < 0.8) {
+      resolve("I am your friend !");
+    } else {
+      reject(new Error("I am not your friend :("));
+    }
+  });
+}
 
-const promesaRechazada = Promise.reject('Error')
-promesaRechazada.catch(err => console.log('Promesa rechazada capturada:', err))
-console.log(promesaRechazada) // Promise { <rejected>: "Error" }
+const response = promiseFriendship();
+
+response.then((res) => console.log(res)).catch((error) => console.error(error));
